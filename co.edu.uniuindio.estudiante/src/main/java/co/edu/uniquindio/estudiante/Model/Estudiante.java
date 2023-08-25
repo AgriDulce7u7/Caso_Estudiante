775ca21cb9aabded10e1b6ceb897fd7fa12f40b9
AgriDulce7u7 <142194231+AgriDulce7u7@users.noreply.github.com>
2023-08-25 13:52:59 -0500

    Creación de variable calcular definitiva

--- co.edu.uniuindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Model/Estudiante.java
+++ co.edu.uniuindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Model/Estudiante.java
@@ -5,25 +5,8 @@ public class Estudiante {
     private int age;
     private String mail;
     private int semester;
-    private  double grades01;
+    private double grades01;
     private double grades02;
-
-    public double getGrades02() {
-        return grades02;
-    }
-
-    public void setGrades02(double grades02) {
-        this.grades02 = grades02;
-    }
-
-    public double getGrades03() {
-        return grades03;
-    }
-
-    public void setGrades03(double grades03) {
-        this.grades03 = grades03;
-    }
-
     private double grades03;
 
     public String getName() {
@@ -64,5 +47,21 @@ public class Estudiante {
 
     public void setGrades01(double grades01) {
         this.grades01 = grades01;
+    }
+
+    public double getGrades02() {
+        return grades02;
+    }
+
+    public void setGrades02(double grades02) {
+        this.grades02 = grades02;
+    }
+
+    public double getGrades03() {
+        return grades03;
+    }
+
+    public void setGrades03(double grades03) {
+        this.grades03 = grades03;
     }
 }
